f02a36f92058d59568258eb321ab89ded8b28d4a
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-12-09 20:42:37 -0500

    center diagram box & position dimension labels

--- src/FormContainer/FormSection/Diagram/Diagram.jsx
+++ src/FormContainer/FormSection/Diagram/Diagram.jsx
@@ -7,35 +7,40 @@ import { Stage, Layer, Rect, Line, Text } from 'react-konva';
 
   let width = state.selectedScreen.Width;
   let height = state.selectedScreen.Height;
+  let canvasWidth = window.innerWidth / 2;
+  let canvasHeight = window.innerHeight - 250;
 
   switch (true) {
     case width > 500:
-      width /= 1.5;
-      height /= 1.5;
+      width /= 1.85;
+      height /= 1.85;
       break;
     case width > 100:
-      width *= 3;
-      height *= 3;
+      width *= 2.75;
+      height *= 2.75;
       break;
     case width > 50:
-      width *= 6;
-      height *= 6;
+      width *= 4;
+      height *= 4;
       break;
     case width > 20:
-      width *= 8;
-      height *= 8;
+      width *= 6;
+      height *= 6;
       break;
     default:
       width *= 25;
       height *= 25;
   }
 
+  let centerWidth = (canvasWidth - width) / 2;
+  let centerHeight = (canvasHeight - height) / 2;
+
   return (
-    <Stage width={window.innerWidth / 2} height={window.innerHeight - 250}>
+    <Stage width={canvasWidth} height={canvasHeight}>
       <Layer>
         <Rect
-          x={50}
-          y={50}
+          x={centerWidth}
+          y={centerHeight}
           width={width}
           height={height}
           fill="white"
@@ -43,43 +48,49 @@ import { Stage, Layer, Rect, Line, Text } from 'react-konva';
           strokeWidth={2}
         />
 
-        <Line
-          points={[50, 50, 50 + width, 50]}
+        <Rect
+          x={centerWidth + 20}
+          y={centerHeight + 20}
+          width={width - 40}
+          height={height - 40}
+          fill="white"
           stroke="black"
           strokeWidth={1}
-          dash={[4, 2]}
-        />
-        <Line
-          points={[50, 50, 50, 50 + height]}
-          stroke="black"
-          strokeWidth={1}
-          dash={[4, 2]}
-        />
-        <Line
-          points={[50 + width, 50, 50 + width, 50 + height]}
-          stroke="black"
-          strokeWidth={1}
-          dash={[4, 2]}
-        />
-        <Line
-          points={[50, 50 + height, 50 + width, 50 + height]}
-          stroke="black"
-          strokeWidth={1}
-          dash={[4, 2]}
+          dash={[10, 10]}
         />
 
+        {/* Screen Width (Position: Top Center) */}
+        <Rect
+          x={centerWidth + width / 2 - 28} 
+          y={centerHeight - height / 4 - 28}
+          width={60}
+          height={40}
+          fill="gray"
+          stroke="black"
+          strokeWidth={1}
+        />
         <Text 
-          x={50 + width / 2 - 19} 
-          y={30} 
-          text={`${state.selectedScreen.Width}` + ` "`} 
+          x={centerWidth + width / 2 - 18} 
+          y={centerHeight - height / 4 - 15}
+          text={`${state.selectedScreen.Width}` + `"`} 
           fontSize={14} 
           fill="black" 
         />
 
+        {/* Screen Height (Position: Right Center) */}
+        <Rect
+          x={centerWidth + width * 1.1} 
+          y={centerHeight + height / 2 - 25}
+          width={60}
+          height={40}
+          fill="gray"
+          stroke="black"
+          strokeWidth={1}
+        />
         <Text 
-          x={width + 56} 
-          y={50 + height / 2 - 8} 
-          text={`${state.selectedScreen.Height}` + ` "`} 
+          x={centerWidth + width * 1.1 + 10} 
+          y={centerHeight + height / 2 - 11}
+          text={`${state.selectedScreen.Height}` + `"`} 
           fontSize={14} 
           fill="black" 
         />
