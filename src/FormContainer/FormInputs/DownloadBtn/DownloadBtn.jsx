2ed81bc86936475b60cf17d866453b92e7b0cb3f
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-12-14 00:00:28 -0500

    download button icon

--- src/FormContainer/FormInputs/DownloadBtn/DownloadBtn.jsx
+++ src/FormContainer/FormInputs/DownloadBtn/DownloadBtn.jsx
@@ -14,6 +14,9 @@ function DownloadBtn() {
       <button onClick={() => saveComponentsAsPdf(componentIds)}>
         Download
       </button>
+      <div className="led-screen-download-icon">
+
+      </div>
     </div>
   )
 }
